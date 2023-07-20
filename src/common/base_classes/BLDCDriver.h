d54df35edfb391eba16c71cf2ca873232ac5dec8
Yan Ke <yanke2@espressif.com>
2023-07-20 11:20:21 +0800

    remove Arduino.h and replace it with esp_platform.h to fit esp_simplefoc

--- src/common/base_classes/BLDCDriver.h
+++ src/common/base_classes/BLDCDriver.h
@@ -1,8 +1,16 @@
 #ifndef BLDCDRIVER_H
 #define BLDCDRIVER_H
 
-#include "Arduino.h"
-#include "FOCDriver.h"
+#include "esp_platform.h"
+
+
+enum PhaseState : uint8_t {
+  PHASE_OFF = 0, // both sides of the phase are off
+  PHASE_ON = 1,  // both sides of the phase are driven with PWM, dead time is applied in 6-PWM mode
+  PHASE_HI = 2,  // only the high side of the phase is driven with PWM (6-PWM mode only)
+  PHASE_LO = 3,  // only the low side of the phase is driven with PWM (6-PWM mode only)
+};
+
 
 class BLDCDriver: public FOCDriver{
     public:
